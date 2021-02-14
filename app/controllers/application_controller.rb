135cd83d02fdcf24137b1f369a2bae1a4b5af814
Renato Marques <renatomarques.contato@gmail.com>
2021-02-14 14:53:01 +0000

    Configurado pundit para criar novo usuário somente quem for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
 
   include SessionsHelper
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+     flash[:danger] = 'Você não tem permissão para fazer esta ação'
+     redirect_to(request.referrer || root_path)
+   end
 end
